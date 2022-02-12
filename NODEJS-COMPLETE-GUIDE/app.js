e84b80ddd3c3f44c9b6fe783eaa878dd3d1c8b44
unknown <locphfx11236@funix.edu.vn>
2022-02-12 16:17:06 +0700

    LAB02.14: Serving Files Statically

--- NODEJS-COMPLETE-GUIDE/app.js
+++ NODEJS-COMPLETE-GUIDE/app.js
@@ -9,7 +9,19 @@ const rootDir = require('./util/path');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(
+    bodyParser.urlencoded(
+        {extended: false}
+    )
+);
+app.use(
+    express.static(
+        path.join(
+            rootDir,
+            'public'
+        )
+    )
+);
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
