fe13cdbf83b45884121693929c878e0c02743a22
unknown <locphfx11236@funix.edu.vn>
2022-02-15 22:45:00 +0700

    LAB03.4: Converting HTML Files to Pug

--- NODEJS-COMPLETE-GUIDE/routes/admin.js
+++ NODEJS-COMPLETE-GUIDE/routes/admin.js
@@ -10,13 +10,7 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(
-        path.join(
-            rootDir,
-            'views',
-            'add-product.html'
-        )
-    );
+    res.render('add-product', {pageTitle: 'Add Product'});
 });
 
 // /admin/product => POST
