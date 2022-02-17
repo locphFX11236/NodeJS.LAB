82261b05997bf379da4321400860db59cc97e76f
unknown <locphfx11236@funix.edu.vn>
2022-02-17 17:20:42 +0700

    LAB04.3: Adding a Product Model

--- NODEJS-COMPLETE-GUIDE/controllers/products.js
+++ NODEJS-COMPLETE-GUIDE/controllers/products.js
@@ -1,12 +1,13 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     res.render(
         'add-product', // Views: all
         {
             pageTitle: 'Add Product', // Views: all
-            path: '/admin/add-product', // Views: all
-    
+            path: '/admin/add-product' // Views: all
+            
+            // , // Views: hbs
             // formsCSS: true, // Views: hbs
             // productCSS: true, // Views: hbs
             // activeAddProduct: true // Views: hbs
@@ -15,11 +16,13 @@ exports.getAddProduct = (req, res, next) => {
 }
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
 }; // Views: all
 
 exports.getProducts = (req, res, next) => {
+    const products = Product.fetchAll(); // Views: all
     res.render(
         'shop',
         {
