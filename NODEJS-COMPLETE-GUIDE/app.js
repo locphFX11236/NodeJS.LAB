31f0dab0dd66d6e236a10d764d5227f02004cd39
unknown <locphfx11236@funix.edu.vn>
2022-02-19 14:38:28 +0700

    //Update Source Code

--- NODEJS-COMPLETE-GUIDE/app.js
+++ NODEJS-COMPLETE-GUIDE/app.js
@@ -2,44 +2,19 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const expressHbs = require('express-handlebars'); // Views: hbs
 
-const rootDir = require('./util/path');
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 
 const app = express();
 
-// app.set('view engine', 'pug'); // Views: pug
-
-// app.engine(
-//   'hbs',
-//   expressHbs({
-//     layoutsDir: 'views/layouts/',
-//     defaultLayout: 'main-layout',
-//     extname: 'hbs'
-//   })
-// ); // Views: hbs
-// app.set('view engine', 'hbs'); // Views: hbs
-
-app.set('view engine', 'ejs'); // Views: ejs
-
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(
-  bodyParser.urlencoded(
-    {extended: false}
-  )
-);
-app.use(
-  express.static(
-    path.join(
-      rootDir,
-      'public'
-    )
-  )
-);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
