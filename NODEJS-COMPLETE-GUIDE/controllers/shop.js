a8c1ba26038bcc79fff869ab1fa60b260259af92
unknown <locphfx11236@funix.edu.vn>
2022-02-23 21:25:50 +0700

    LAB05.7: Fetching a Single Product with the where Condition

--- NODEJS-COMPLETE-GUIDE/controllers/shop.js
+++ NODEJS-COMPLETE-GUIDE/controllers/shop.js
@@ -2,7 +2,8 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product
+    .fetchAll()
     .then(([rows, fieldData]) => {
       res.render('shop/product-list', {
         prods: rows,
@@ -16,17 +17,22 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    res.render('shop/product-detail', {
-      product: product,
-      pageTitle: product.title,
-      path: '/products'
-    });
-  });
+  Product
+    .findById(prodId)
+    .then(([product]) => {
+      res.render('shop/product-detail', {
+        product: product[0],
+        pageTitle: product.title,
+        path: '/products'
+      });
+    })
+    .catch()
+  ;
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
+  Product
+    .fetchAll()
     .then(([rows, fieldData]) => {
       res.render('shop/index', {
         prods: rows,
