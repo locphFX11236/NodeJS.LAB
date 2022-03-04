c4c7ffc320309bead430ebb32a693005a8f1d9cb
unknown <locphfx11236@funix.edu.vn>
2022-03-04 22:01:18 +0700

    LAB07.11 -> 13: Cart Function and CRUD Cart

--- NODEJS-COMPLETE-GUIDE/models/user.js
+++ NODEJS-COMPLETE-GUIDE/models/user.js
@@ -26,6 +26,37 @@ const userSchema = new Schema({
     },
 });
 
+userSchema.methods.addToCart = function(product) {
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+        return cp.productId.toString() === product._id.toString();
+    });
+    let newQuality = 1;
+    const updatedCartItems = [ ...this.cart.items ];
+
+    if (cartProductIndex >= 0) {
+        newQuality = this.cart.items[cartProductIndex].quantity + 1;
+        updatedCartItems[cartProductIndex].quantity = newQuality;
+    } else {
+        updatedCartItems.push({ 
+            productId: product._id,
+            quantity: newQuality
+        })
+    }
+    const updatedCart = {
+        items: updatedCartItems
+    };
+    this.cart = updatedCart;
+    return this.save();
+};
+
+userSchema.methods.deleteItemFromCart = function(productId) {
+    const updatedCartItems = this.cart.items.filter(item => {
+        return item.productId.toString() !== productId.toString();
+    });
+    this.cart.items = updatedCartItems;
+    return this.save();
+};
+
 module.exports = mongoose.model('User', userSchema);
 
 // const mongodb = require('mongodb');
