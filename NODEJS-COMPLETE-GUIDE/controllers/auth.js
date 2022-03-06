7ffd1d7da51ffc9eb97e9d3846b92b33cf41fefd
unknown <locphfx11236@funix.edu.vn>
2022-03-06 17:04:26 +0700

    LAB08.1 -> 4: Setting and Using Cookie

--- NODEJS-COMPLETE-GUIDE/controllers/auth.js
+++ NODEJS-COMPLETE-GUIDE/controllers/auth.js
@@ -1,6 +1,20 @@
 exports.getLogin = (req, res, next) => {
+    // console.log(req.get('Cookie'));
+    const isLoggedIn = req
+        .get('Cookie')
+        // .split(';')[0] // Neu Cookie co nhieu tham so danh chi muc split[n] de lay gia tri "loggedIn=true"
+        .trim() // Loai bo khoang trong du thua
+        .split('=')[1] // Chi chon text co gia tri la 'true'
+        === 'true'
+    ;
     res.render('auth/login', {
         path: '/login',
-        pageTitle: 'Login'
+        pageTitle: 'Login',
+        isAuthenticated: isLoggedIn
     });
-  };
+};
+
+exports.postLogin = (req, res, next) => {
+    res.setHeader('Set-Cookie', 'loggedIn=true');
+    res.redirect('/');
+};
