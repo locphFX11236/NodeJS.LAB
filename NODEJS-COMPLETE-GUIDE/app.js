8f13c71e8eb0ae5e9754ec54e40685e2c75c3d61
unknown <locphfx11236@funix.edu.vn>
2022-03-08 10:39:06 +0700

    LAB08.14: Setting and Implementing an Authentication Flow

--- NODEJS-COMPLETE-GUIDE/app.js
+++ NODEJS-COMPLETE-GUIDE/app.js
@@ -58,21 +58,6 @@ app.use(errorController.get404);
 mongoose
     .connect(MONGODB_URI)
     .then(result => {
-        User
-            .findOne()
-            .then(user => {
-                if (!user) {
-                    const user = new User({
-                        name: 'Loc',
-                        email: 't@t.com',
-                        cart: {
-                            items: []
-                        }
-                    });
-                    user.save();
-                }
-            })
-        ;
         app.listen(3000);
     })
     .catch(err => {
