96e7728c9afcb90788ba4415f1321e914a014c4b
unknown <locphfx11236@funix.edu.vn>
2022-03-10 16:20:08 +0700

    LAB09.1 & 2: Setting Validation and Validating Email

--- NODEJS-COMPLETE-GUIDE/controllers/auth.js
+++ NODEJS-COMPLETE-GUIDE/controllers/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const { validationResult } = require('express-validator');
 
 const User = require('../models/user');
 
@@ -70,6 +71,15 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg
+    });
+  }
   if (confirmPassword !== password) {
     req.flash('error', 'Confirm Password and Password are not match.');
     return res.redirect('/signup');
--- NODEJS-COMPLETE-GUIDE/routes/auth.js
+++ NODEJS-COMPLETE-GUIDE/routes/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check } = require('express-validator');
 
 const authController = require('../controllers/auth');
 
@@ -7,7 +8,14 @@ const router = express.Router();
 router.get('/login', authController.getLogin);
 router.get('/signup', authController.getSignup);
 router.post('/login', authController.postLogin);
-router.post('/signup', authController.postSignup);
+router.post(
+    '/signup',
+    check('email') // Xac thuc email
+        .isEmail()
+        .withMessage('Please enter a valid email.') // Ran gia tri vao errors.array()[0].msg
+    ,
+    authController.postSignup
+);
 router.post('/logout', authController.postLogout);
 
 module.exports = router;
