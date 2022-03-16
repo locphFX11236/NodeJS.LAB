24b0534a60034eaa7ee2ef38d4fa7acd666ad646
unknown <locphfx11236@funix.edu.vn>
2022-03-16 17:48:07 +0700

    LAB10.7 -> 14:  Downloading Order Files, Generating .pdf Files with Order Data

--- NODEJS-COMPLETE-GUIDE/routes/shop.js
+++ NODEJS-COMPLETE-GUIDE/routes/shop.js
@@ -21,6 +21,8 @@ router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
 
 router.post('/create-order', isAuth, shopController.postOrder);
 
-router.get('/orders', isAuth, shopController.getOrders);;
+router.get('/orders', isAuth, shopController.getOrders);
+
+router.get('/order/:orderId', isAuth, shopController.getInvoice);
 
 module.exports = router;
